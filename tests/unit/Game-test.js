371bd719e6e647efa85d985af9739103b737ee7d
Tom Dickman <twdickman@gmail.com>
2021-02-11 23:21:05 +1100

    add additional test coverage
    - Add coverage for methods determining if previous frames were strikes/spares
    - Add additional coverage for scoring to test a complete Game

--- tests/unit/Game-test.js
+++ tests/unit/Game-test.js
@@ -36,9 +36,103 @@ describe('Game', () => {
     describe('score', () => {
         it('should accurately total all frame scores including spares and strikes', () => {
             const game = new Game('unit test');
+            // Frame #1.
             assert.equal(game.score(), 0);
             game.roll(4);
             assert.equal(game.score(), 4);
+            game.roll(4);
+            assert.equal(game.score(), 8);
+            // Frame #2.
+            game.roll(10);
+            assert.equal(game.score(), 18);
+            // Frame #3.
+            game.roll(10);
+            assert.equal(game.score(), 38);
+            // Frame #4.
+            game.roll(10);
+            assert.equal(game.score(), 68);
+            // Frame #5.
+            game.roll(3);
+            assert.equal(game.score(), 77);
+            game.roll(7);
+            assert.equal(game.score(), 91);
+            // Frame #6.
+            game.roll(6);
+            assert.equal(game.score(), 103);
+            game.roll(3);
+            assert.equal(game.score(), 106);
+            // Frame #7.
+            game.roll(10);
+            assert.equal(game.score(), 116);
+            // Frame #8.
+            game.roll(1);
+            assert.equal(game.score(), 118);
+            game.roll(1);
+            assert.equal(game.score(), 120);
+            // Frame #9.
+            game.roll(10);
+            assert.equal(game.score(), 130);
+            // Frame #10.
+            game.roll(10);
+            assert.equal(game.score(), 150);
+            game.roll(10);
+            assert.equal(game.score(), 170);
+            game.roll(10);
+            assert.equal(game.score(), 180);
+        });
+    });
+
+    describe('isPrevFrameSpareRollRequired', () => {
+        it('should determine previous frame requires spare roll if it was a spare', () => {
+            const game = new Game('unit test').roll(2).roll(8);
+            const currentFrame = game.frames[game.currentFrameNumber];
+            const prevFrame = game.frames[game.currentFrameNumber - 1];
+            assert.isTrue(game.isPrevFrameSpareRollRequired(currentFrame, prevFrame));
+        });
+
+        it('should determine previous frame does not require spare roll if it was not a spare', () => {
+            const game = new Game('unit test').roll(10);
+            const currentFrame = game.frames[game.currentFrameNumber];
+            const prevFrame = game.frames[game.currentFrameNumber - 1];
+            assert.isFalse(game.isPrevFrameSpareRollRequired(currentFrame, prevFrame));
+        });
+    });
+
+    describe('isPrevFrameStrikeRollRequired', () => {
+        it('should determine previous frame requires strike roll if it was a strike', () => {
+            const game = new Game('unit test').roll(10);
+            const prevFrame = game.frames[game.currentFrameNumber - 1];
+            assert.isTrue(game.isPrevFrameStrikeRollRequired(prevFrame));
+        });
+
+        it('should determine previous frame does not require strike roll if it was not a strike', () => {
+            const game = new Game('unit test').roll(8).roll(2);
+            const currentFrame = game.frames[game.currentFrameNumber];
+            const prevFrame = game.frames[game.currentFrameNumber - 1];
+            assert.isFalse(game.isPrevFrameStrikeRollRequired(currentFrame, prevFrame));
+        });
+    });
+
+    describe('isFrameBeforeLastStrikeRollRequired', () => {
+        it('should determine frame before last requires strike roll if it was a strike and previous frame was a strike', () => {
+            const game = new Game('unit test').roll(10).roll(10);
+            const prevFrame = game.frames[game.currentFrameNumber - 1];
+            const frameBeforeLast = game.frames[game.currentFrameNumber - 2];
+            assert.isTrue(game.isFrameBeforeLastStrikeRollRequired(prevFrame, frameBeforeLast));
+        });
+
+        it('should determine frame before last does not require strike roll if it was not a strike', () => {
+            const game = new Game('unit test').roll(8).roll(2).roll(10);
+            const prevFrame = game.frames[game.currentFrameNumber - 1];
+            const frameBeforeLast = game.frames[game.currentFrameNumber - 2];
+            assert.isFalse(game.isFrameBeforeLastStrikeRollRequired(prevFrame, frameBeforeLast));
+        });
+
+        it('should determine frame before last does not require strike roll if previous frame was not a strike', () => {
+            const game = new Game('unit test').roll(10).roll(3).roll(7);
+            const prevFrame = game.frames[game.currentFrameNumber - 1];
+            const frameBeforeLast = game.frames[game.currentFrameNumber - 2];
+            assert.isFalse(game.isFrameBeforeLastStrikeRollRequired(prevFrame, frameBeforeLast));
         });
     });
 });
